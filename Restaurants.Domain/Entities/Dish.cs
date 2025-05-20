f9b6a96daa8df3b581f65d333fd6321a63d08f69
Isaac M. Vásquez <isaacvm7809@gmail.com>
2025-05-20 17:18:16 -0600

    EFStep1

--- Restaurants.Domain/Entities/Dish.cs
+++ Restaurants.Domain/Entities/Dish.cs
@@ -6,5 +6,7 @@
         public string Name { get; set; } = default!;    
         public string Description { get; set; } = default!;
         public decimal Price { get; set; } 
+
+        public Guid RestaurantId { get; set; }
     }
 }
---
+++ Restaurants.Infrastructure/Persistence/RestaurantDbContext.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Restaurants.Domain.Entities;
+using Restaurants.Infrastructure.Migrations;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Diagnostics.Metrics;
+
+namespace Restaurants.Infrastructure.Persistence
+{
+    internal class RestaurantDbContext  : DbContext
+    {
+        internal DbSet<Restaurant> Restaurants { get; set; }
+        internal DbSet<Dish> Dishes { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //optionsBuilder.UseSqlServer(@"Server=localhost\SQLEXPRESS;Database=RestaurantsDb;Integrated Security=SSPI;Persist Security Info=True;");
+            optionsBuilder.UseSqlServer(@"Integrated Security=SSPI;TrustServerCertificate=True;Persist Security Info=False;Initial Catalog=RestaurantsDb;Data Source=localhost\SQLEXPRESS");
+            
+
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Restaurant>()
+                .OwnsOne(r => r.Address);
+
+            modelBuilder.Entity<Restaurant>()
+                .HasMany(r => r.Dishes)
+                .WithOne()
+                .HasForeignKey(d => d.RestaurantId);
+
+
+        }
+
+
+    }
+}
